efb6a8f1352c147c24e8c4b92ba7bfca7e266967
zaprlnn <ibnuzaprln@gmail.com>
2024-11-04 23:26:20 +0700

    prak 5

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
+  
